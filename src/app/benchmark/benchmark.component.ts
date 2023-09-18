89aa99153c61fc943864e1de9449e23f01123bcf
nasimakhtar <nasim@backbase.com>
2023-09-18 09:36:12 +0530

    refactored code for stopwatch

--- src/app/benchmark/benchmark.component.ts
+++ src/app/benchmark/benchmark.component.ts
@@ -45,14 +45,14 @@ export class BenchmarkComponent implements OnInit {
     const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60))
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-    const mseconds = Math.floor((elapsedTime % 1000) / 10);
+    const mseconds = Math.floor((elapsedTime % 1000));
     return (
       (hours ? (hours > 9 ? hours : "0" + hours) : "00")
       + ":" +
       (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00")
       + ":" +
       (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00")
-      + "." +
+      + ":" +
       (mseconds > 9 ? mseconds : "0" + mseconds));
   }
 }
